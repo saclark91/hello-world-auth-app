e01b98ae5a57d27d04d007ead6ccd5b72dff671b
Stephen Clark <saclark918@outlook.com>
2024-11-01 23:27:56 -0400

    Added / route

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
+// Root route
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to MongoDB"))
